7d36cf6909e57ffee95b81fb265efe4ad3bda0da
MANCHI SAI CHANDRA <90021293+MANCHISAICHANDRA@users.noreply.github.com>
2023-05-31 17:53:30 +0530

    Update voter.php

--- voter.php
+++ voter.php
@@ -48,6 +48,28 @@
 		
 	  </div>
 	</nav>
+	  
+	<?php
+	  // If Login is true this will show an success popup
+	  if($login){
+	  echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
+	    <strong>Success!</strong> You are logged in.
+	    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+		<span aria-hidden="true">&times;</span>
+	    </button>
+	    </div>';
+	  }
+	  // If there is any error this will show an error popup.
+	  if($showError){
+	    echo '<div class="alert alert-danger alert-dismissible fade show" role="alert">
+	      <strong>Error!</strong> '. $showError.'
+	      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+		  <span aria-hidden="true">&times;</span>
+	      </button>
+	      </div>';
+	  }
+	?>
+  
 
 	<div id="login">
        <!-- Form where we provide voter username and password. --> 
