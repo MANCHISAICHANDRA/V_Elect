4da77f2603cc522f1394306159cf364f8f4fdd46
MANCHI SAI CHANDRA <90021293+MANCHISAICHANDRA@users.noreply.github.com>
2023-05-31 16:42:21 +0530

    Update voters in database

--- add_voter.php
+++ add_voter.php
@@ -1,4 +1,42 @@
+<?php
+//Data that we put in the form will going to check in the database if same username exist they will tell Username alreay exists.
+// If the username is not present the data will going to add in a voter table. 
 
+$showAlert = false;
+$showError = false;
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    //Connect you to the database.
+	include '_dbconnect.php';
+	//Post the values that we get in the form
+	$fname = $_POST["fname"];
+    $lname = $_POST["lname"];
+    $gender = $_POST["gender"];
+    $branch = $_POST["branch"];
+    $year = $_POST["year"];
+	$enrollment = $_POST["enrollment"];
+	$username = $_POST["username"];
+	$password = $_POST["password"];
+	$cpassword = $_POST["cpassword"];
+    // Check the username already exist or not.
+    $existSql = "SELECT * FROM `voter` WHERE username = '$username'";
+    $result = mysqli_query($conn, $existSql);
+    $numExistRows = mysqli_num_rows($result);
+    if($numExistRows > 0){
+        $showError = "Username Already exists.";
+    }else{
+       if($password == $cpassword){
+            $sql = "INSERT INTO `voter` (`fname`, `lname`, `gender`, `branch`, `year`, `enrollment`, `username`, `password`) VALUES ('$fname', '$lname', '$gender', '$branch', '$year', '$enrollment', '$username', '$password')";
+            $result = mysqli_query($conn, $sql);
+            if($result){
+                $showAlert = true;
+            }
+        }
+        else{
+            $showError = "Passwords do not Match.";
+        }
+}
+}
+?>
 
 <!doctype html>
 <html lang="en">
